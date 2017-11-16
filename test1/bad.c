7bd97b2f9793dace6eaa24c4150d7cbd5c7cced5
Kenneth High <kennethhigh7@gmail.com>
2017-11-16 03:02:16 -0800

    Add files via upload

---
+++ test1/bad.c
@@ -0,0 +1,5 @@
+/* $Id: bad.c,v 1.3 2015-11-29 22:48:39-08 - - $ */
+
+This is a bad file.
+cc won't compile it.
+
---
+++ test1/hello.c
@@ -0,0 +1,7 @@
+/* $Id: hello.c,v 1.3 2015-11-29 22:48:39-08 - - $ */
+
+int main (void) {
+   printf ("Hello, world\n");
+   return 0;
+}
+
